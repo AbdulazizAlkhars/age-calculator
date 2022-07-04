90d8ea3e82ab4ae3c84f50682dc6a5b95333f8e2
AbdulazizAlkhars <alkharswork@gmail.com>
2022-07-04 21:25:00 +0300

    Age Calculator Task Done

--- age_calculator.py
+++ age_calculator.py
@@ -1,19 +1,35 @@
-from datetime import date
+from datetime import date, datetime
+from xml.etree.ElementPath import prepare_descendant
+
+
+year = int(input("Enter year of birth:"))
+mont = int(input("Enter month of birth:"))
+day = int(input("Enter day of birth:"))
+
+prisentDate = date.today()
 
 
 def get_dob():
     # write code here
-	...
+    dob = date(year, mont, day)
+    return dob
 
 
 def get_age(dob):
     # write code here
-	...
+    age = prisentDate - dob
+    return age
 
 
 def main():
-	# write code here
-	...
+    # write code here
+
+    dateOfBirth = get_dob()
+    age = int((prisentDate - dateOfBirth).days / 365)
+    if prisentDate > dateOfBirth:
+        print(f"You are {age} years old")
+    else:
+        print("Invalid Date")
 
 
 if __name__ == '__main__':
